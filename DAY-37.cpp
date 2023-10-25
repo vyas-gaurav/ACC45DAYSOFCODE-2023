5c4d57a20db875c0c332c53cb597269f9b1da3e7
Gaurav Vyas <140307480+vyas-gaurav@users.noreply.github.com>
2023-10-25 10:23:42 +0530

    Create DAY-37.cpp

---
+++ DAY-37.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n==1 || n%2==0)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
